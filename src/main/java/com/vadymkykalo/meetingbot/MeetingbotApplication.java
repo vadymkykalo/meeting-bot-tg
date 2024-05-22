ad2c6aaabf99998d62f630faeff793ef1df36ba0
vadymkykalo <vadymkykalo@gmail.com>
2024-05-22 17:46:24 +0300

    fixed property

--- src/main/java/com/vadymkykalo/meetingbot/MeetingbotApplication.java
+++ src/main/java/com/vadymkykalo/meetingbot/MeetingbotApplication.java
@@ -1,5 +1,6 @@
 package com.vadymkykalo.meetingbot;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MeetingbotApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> {
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
+
 		SpringApplication.run(MeetingbotApplication.class, args);
 	}
 
